262d794a96579032a75d4e3abf98b3c83a446af0
Avinash <>
2022-03-24 17:37:06 +0530

    feat:updated project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router';
 import Contact from './Pages/Contact';
 import Home from './components/Home';
 import About from './Pages/About';
+import Login from './Pages/Login';
 import './App.css';
 
 
@@ -16,7 +17,8 @@ function App() {
       <Route path = "/" element = {<Home />} />
       <Route path="contact" element={<Contact />} />
       <Route path="about" element={<About />} />
-      <Route path="login" element={<Contact />} />
+      <Route path="login" element={<Login />} />
+      
       </Routes>
       
       
