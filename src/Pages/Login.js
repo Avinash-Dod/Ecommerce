0dfc4d8a1400cd6ca4cc6d18764221d5d611b226
Avinash <>
2022-03-29 17:57:54 +0530

    feat:how to fetch api using fake api

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,44 @@
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import {  useState } from "react";
 import './Login_signup.css'
-const st ={
-  "max-width":"500px",
-  "margin":"auto",
-  "height":"fit-content",
+const st = {
+  "max-width": "500px",
+  "margin": "auto",
+  "height": "fit-content",
 }
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+
+function userNameHandler(event) {
+  setUsername(event.target.value);
+}
+function passwordHandler(event) {
+  setPassword(event.target.value);
+}
+function onSubmitHandler(event) {
+  event.preventDefault();
+  const userData = {
+    userName: username,
+    password: password,
+
+  };
+
+  console.log(userData)
+  setUsername('');
+  setPassword('');
+
+  const Api=async()=>{
+    const result=await fetch('https://fakestoreapi.com/products/1')
+    console.log(result)
+    
+  }
+  Api();
+};
+
 
   return (
     <>
@@ -18,29 +48,29 @@ const Login = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-12">
-              <div className="login">
+              <div className="login ">
                 <div className="Head">
-              <h2>Login Form</h2>
-              </div>
-                <form style={st}>
-                  
+                  <h2>Login Form</h2>
+                </div>
+                <form style={st} onSubmit={onSubmitHandler}>
+
                   <div className="input-container well">
                     <i className="fa fa-user icon"></i>
-                    <input className="input-field" type="text" placeholder="Username" name="usrnm" />
+                    <input className="input-field" type="text" placeholder="Username" name="username" onChange={userNameHandler} value={username} />
                   </div>
 
-                  
+
 
                   <div className="input-container">
                     <i className="fa fa-key icon"></i>
-                    <input className="input-field" type="password" placeholder="Password" name="psw" />
+                    <input className="input-field" type="password" placeholder="Password" name="psw" onChange={passwordHandler} value={password} />
                   </div>
 
                   <button type="submit" className="btn primary-btn">Login </button>
-                 <div className="link-register">
-                  <span class="psw">Not a member? <a href=" #"><Link to="/signup">Register</Link></a></span>
-     <span class="psw"> Forgot <a href=" #">Password?</a></span>
-     </div>
+                  <div className="link-register">
+                    <span class="psw">Not a member? <a href=" #"><Link to="/signup">Register</Link></a></span>
+                    <span class="psw"> Forgot <a href=" #">Password?</a></span>
+                  </div>
                 </form>
               </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,44 @@
 import { Link } from "react-router-dom";
 
 function Header() {
-    return (
-      <>
-     <header class="">
-      <nav class="navbar navbar-expand-lg">
-        <div class="container">
-          <a class="navbar-brand" href="index.html"><h2><i class="fa fa-shopping-bag" aria-hidden="true"></i> Imperial <em>Shopping</em></h2></a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarResponsive">
-           <ul className="navbar-nav ml-auto">
-              <li className="nav-item active ">
-              <Link className="nav-link" to="/" ><i class="fa fa-home" aria-hidden="true"></i> Home</Link>
-              </li> 
-             
-              <li className="nav-item">
-              <Link className="nav-link" to="/contact"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/about"><i class="fa fa-info-circle" aria-hidden="true"></i> About</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/login"><i class="fa fa-user" aria-hidden="true"></i> Login</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</Link>
-              </li>
-              <li className="nav-item">
-              <Link className="nav-link" to="/favourite"><i class="fa fa-heart" aria-hidden="true"></i> Favourite</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
+  return (
+    <>
+      <header class="">
+        <nav class="navbar navbar-expand-lg">
+          <div class="container">
+            <a class="navbar-brand" href="index.html"><h2><i class="fa fa-shopping-bag" aria-hidden="true"></i> Imperial <em>Shopping</em></h2></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item active ">
+                  <Link className="nav-link" to="/" ><i class="fa fa-home" aria-hidden="true"></i> Home</Link>
+                </li>
 
-      </>
-    )
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact"><i class="fa fa-envelope" aria-hidden="true"></i> Contact</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about"><i class="fa fa-info-circle" aria-hidden="true"></i> About</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login"><i class="fa fa-user" aria-hidden="true"></i> Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/cart"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/favourite"><i class="fa fa-heart" aria-hidden="true"></i> Favourite</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+    </>
+  )
 }
 export default Header;
 
